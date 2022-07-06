15aa375cd2c8939cd223f6b25f7e379c2c037034
Robert Biddle <RobBiddle@users.noreply.github.com>
2022-07-06 09:06:47 -0400

    Update Invoke-LogoffDisconnectedAdmins.ps1
    Reversing the switch parameter and removing else statement so that both scenarios can be used on connection brokers

--- Invoke-LogoffDisconnectedAdmins.ps1
+++ Invoke-LogoffDisconnectedAdmins.ps1
@@ -4,8 +4,8 @@
 .DESCRIPTION
     Looks for disconnected user sessions. If found checks to determine if the user has local admin rights.
     If the disconnected Desktop session belongs to an Admin user then a logoff is performed for the session.
-    On Remote Desktop Connection Broker servers this will impact all servers in the RD Deployment. This behavior
-    can be prevented by using the -IgnoreRDSession switch parameter.
+    This cmdlet only affects connections to the localhost by default. If -IncludeRDSessions is specified then 
+    if the localhost is a Remote Desktop Connection Broker servers this will impact all servers in the RD Deployment.
 .NOTES
     Author: Robert D. Biddle
     Created: 2022-07-05
@@ -13,12 +13,12 @@
     https://github.com/RobBiddle/Invoke-LogoffDisconnectedAdmins
 .EXAMPLE
     Invoke-LogoffDisconnectedAdmins
-    Invoke-LogoffDisconnectedAdmins -IgnoreRDSession
+    Invoke-LogoffDisconnectedAdmins -IncludeRDSessions
 #>
 [CmdletBinding()]
 param (
-    # Switch to prevent logoff via RDUserSession on Connection Brokers and force local session detection
-    [switch]$IgnoreRDSession
+    # Switch to logoff via RDUserSession on Connection Brokers in addition to localhost sessions
+    [switch]$IncludeRDSessions
 )
 
 function IsUserAdmin ($user, $domain) {
@@ -42,7 +42,7 @@ function IsUserSessionDisconnected ($user) {
 
 # Connection Brokers allow for logging off RD Sessions quickly so that is preferred
 $isConnectionBroker = (Get-WindowsFeature RDS-Connection-Broker | Where-Object Installed).count -gt 0
-if ($isConnectionBroker -and (-NOT $IgnoreRDSession)) {
+if ($isConnectionBroker -and $IncludeRDSessions) {
     $DisconnectedAdminDesktopSessions = @()
     $DisconnectedSessions = Get-RDUserSession | Where-Object SessionState -eq 'STATE_DISCONNECTED'
     foreach ($ds in $DisconnectedSessions) {
@@ -60,52 +60,49 @@ if ($isConnectionBroker -and (-NOT $IgnoreRDSession)) {
         }
     }
 }
-else {
-    # All Logged on Users, includes service accounts and network connections
-    $Win32_LoggedOnUsers = Get-CimInstance -Class Win32_LoggedOnUser | Select-Object Antecedent, Dependent -Unique
-    # Limit to matching Remote Desktop Sessions -- Connection state is not known at this point
-    $DesktopSessionLogonIds = ((Get-CimInstance -Class Win32_LogonSession | Where-Object LogonId -in $Win32_LoggedOnUsers.Dependent.LogonId | Where-Object LogonType -eq 10)).LogonId
-    $DesktopSessions = $Win32_LoggedOnUsers | Where-Object { $_.Dependent.LogonId -in $DesktopSessionLogonIds }
 
-    $DisconnectedAdminDesktopSessions = @()
-    foreach ($ds in $DesktopSessions) {
-        
+# All Logged on Users, includes service accounts and network connections
+$Win32_LoggedOnUsers = Get-CimInstance -Class Win32_LoggedOnUser | Select-Object Antecedent, Dependent -Unique
+# Limit to matching Remote Desktop Sessions -- Connection state is not known at this point
+$DesktopSessionLogonIds = ((Get-CimInstance -Class Win32_LogonSession | Where-Object LogonId -in $Win32_LoggedOnUsers.Dependent.LogonId | Where-Object LogonType -eq 10)).LogonId
+$DesktopSessions = $Win32_LoggedOnUsers | Where-Object { $_.Dependent.LogonId -in $DesktopSessionLogonIds }
 
-        # Determine if user has a disconnected session
-        $UserSessionIsDisconnected = IsUserSessionDisconnected $ds.Antecedent.Name
-        if (-NOT $UserSessionIsDisconnected) {
-            Continue
-        }
+$DisconnectedAdminDesktopSessions = @()
+foreach ($ds in $DesktopSessions) {
+    # Determine if user has a disconnected session
+    $UserSessionIsDisconnected = IsUserSessionDisconnected $ds.Antecedent.Name
+    if (-NOT $UserSessionIsDisconnected) {
+        Continue
+    }
 
-        # Determine if user has local Admin rights
-        $UserIsAdmin = IsUserAdmin $ds.Antecedent.Name $ds.Antecedent.Domain
-        if (-NOT $UserIsAdmin) {
-            Continue
-        }
+    # Determine if user has local Admin rights
+    $UserIsAdmin = IsUserAdmin $ds.Antecedent.Name $ds.Antecedent.Domain
+    if (-NOT $UserIsAdmin) {
+        Continue
+    }
 
-        # Using process metadata to match Win32_LogonSession LogonId to Session ID of console/terminal session
-        $dsProcesses = Get-CimInstance -ClassName Win32_SessionProcess | Where-Object { $_.Antecedent.LogonID -eq $ds.Dependent.LogonId }
-        $SessionIdsFromProcesses = @()
-        foreach ($dsp in $dsProcesses) {
-            $SessionIdsFromProcesses += (Get-Process -Id $dsp.Dependent.Handle -IncludeUserName).SessionId
-        }
-        $SessionIdsFromProcesses = $SessionIdsFromProcesses | Select-Object -Unique
-        if ($SessionIdsFromProcesses.count -gt 1) {
-            Write-Warning "Multiple session IDs found. This is unexpected."
-            Get-Process -Id $dsp.Dependent.Handle -IncludeUserName
-        }
-        else {
-            $s = $SessionIdsFromProcesses
-            if ( query.exe session $s) {
-                $SessionId = $s
-                $DisconnectedAdminDesktopSessions += [PSCustomObject]@{
-                    UserName  = $ds.Antecedent.Name
-                    Domain    = $ds.Antecedent.Domain
-                    LogonId   = $ds.Dependent.LogonId
-                    SessionId = $SessionId
-                }
-            } 
-        }
+    # Using process metadata to match Win32_LogonSession LogonId to Session ID of console/terminal session
+    $dsProcesses = Get-CimInstance -ClassName Win32_SessionProcess | Where-Object { $_.Antecedent.LogonID -eq $ds.Dependent.LogonId }
+    $SessionIdsFromProcesses = @()
+    foreach ($dsp in $dsProcesses) {
+        $SessionIdsFromProcesses += (Get-Process -Id $dsp.Dependent.Handle -IncludeUserName).SessionId
+    }
+    $SessionIdsFromProcesses = $SessionIdsFromProcesses | Select-Object -Unique
+    if ($SessionIdsFromProcesses.count -gt 1) {
+        Write-Warning "Multiple session IDs found. This is unexpected."
+        Get-Process -Id $dsp.Dependent.Handle -IncludeUserName
+    }
+    else {
+        $s = $SessionIdsFromProcesses
+        if ( query.exe session $s) {
+            $SessionId = $s
+            $DisconnectedAdminDesktopSessions += [PSCustomObject]@{
+                UserName  = $ds.Antecedent.Name
+                Domain    = $ds.Antecedent.Domain
+                LogonId   = $ds.Dependent.LogonId
+                SessionId = $SessionId
+            }
+        } 
     }
 }
 
